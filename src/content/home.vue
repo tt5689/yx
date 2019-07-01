cf449f2d1c1b9c2021426d3abc8564ea458513c7
tt5689 <1529802869@qq.com>
2019-07-01 17:53:35 +0800

    整体框架提交

--- src/content/home.vue
+++ src/content/home.vue
@@ -1,29 +1,18 @@
 <template>
 <div id="wrap">
-    <div id="header_wrap">
-        <Header/>
-        <Nav/>
-    </div>
+    <Home>
 <TabBar/>
 </div>
 </template>
 
 <script>
-import Header from '../camp/header.vue';
-import Nav from '../camp/nav.vue';
+import Home from '../content/home.vue';
 
 export default {
     components:{
-        Header,
-        Nav
+        Home
     }
 }
 </script>
-<style lang="scss" scoped>
-    #header_wrap{
-        width: 100%;
-        position: fixed;
-        top: 0;
-        left: 0;
-    }
+<style>
 </style>
